2df032d0fee5dcbe3beef3aa453b2c6cb24e534b
Cside <cside.story@gmail.com>
2018-03-27 09:47:16 +0900

    rename

--- lib/Util.pm
+++ lib/Util.pm
@@ -1,24 +1,40 @@
-#!/usr/bin/env perl
+package Util;
 use common::sense;
+
 use XML::Atom::Client;
 use URI::Escape qw(uri_escape_utf8);
 use AnyEvent;
 use AnyEvent::HTTP;
 use Data::Dumper::Concise;
 
+my $DEBUG = 1;
+
 sub p($) { warn Dumper $_[0] }
+sub debug($) { say $_[0] if $DEBUG }
 
 my $BASE_URL = 'http://b.hatena.ne.jp/Cside/atomfeed?tag=' . uri_escape_utf8('あとで読む');
 
-my @urls = get_urls();
+my $count = get_unread_count();
+my @urls = get_urls($count);
 my @atoms = fetch_atoms(@urls);
 my @ids = get_ids_by_atoms(@atoms);
 
-sub get_urls {
+sub get_unread_count {
     my $parser = XML::Atom::Client->new;
     my $feed = $parser->getFeed($BASE_URL);
 
     my ($count) = $feed->title =~ /\((\d+)\)$/;
+    if ($count !~ /^[0-9]+$/) {
+        die 'Not a integer';
+    }
+    debug "Current unreads: $count";
+
+    return $count;
+}
+
+sub get_urls {
+    my ($count) = @_;
+
     my $offset = 0;
     my $limit  = 20;
 
@@ -40,7 +56,7 @@ sub fetch_atoms {
     my @atoms; 
     my $cv = AnyEvent->condvar;
     for my $url (@urls) {
-    	say "Start $url";
+    	debug "Start $url";
     	$cv->begin;
     	http_get $url, sub {
     		my ($content, $headers) = @_;
@@ -53,7 +69,7 @@ sub fetch_atoms {
     $cv->recv;
 
     if (scalar(@urls) != scalar(@atoms)) {
-        die 'Error';
+        die 'URL fetch error';
     }
     return @atoms
 }
@@ -69,3 +85,5 @@ sub get_ids_by_atoms {
     }
     return @ids;
 }
+
+1;
